f2abc703b81f3428a0d9e5bc6e539805c470dfa7
mstzturk <mesutoztrk@msn.com>
2016-09-27 09:42:20 +0300

    Hello C# World

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Adınızı giriniz");
+            string girilen = Console.ReadLine();
+            Console.WriteLine("Hoşgeldin " + girilen);
+
+            //Veri tipleri
+            //T A = D
+
+            /*
+             * değişken isimleri sayı ile başlamaz
+             * sistemde tanımlı olan nesneler değişken adı olarak kullanılamaz
+             * özel karakterlerle değişken adı verilemez
+             * 
+             * camelCase PascalCase
+             */
+
+            Console.Read();
+        }
+    }
+}
