d3e2404b6c494a62f2f9d807f43a81d11d24c785
mstzturk <mesutoztrk@msn.com>
2016-09-27 09:49:12 +0300

    variable types

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -26,7 +26,19 @@ namespace HelloWorld
              * 
              * camelCase PascalCase
              */
+            int _degisken = 3;
+            double enBuyukOrtakBolen = 4;
 
+
+            //Tam sayı değişkenleri
+            byte _byte = 255;
+            sbyte _sbyte = -23;
+            short _short = 32423;
+            ushort _ushort = 32423;
+            int _int = 23423432;
+            uint _uint = 4242342343;
+            long _long = 23984729384;
+            ulong _ulong = 223984729384;
             Console.Read();
         }
     }
